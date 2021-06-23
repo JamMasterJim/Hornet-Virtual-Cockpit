6b058609d9d9559595ff1df437733f7fd27ba637
James Hall <62351162+jameshall87@users.noreply.github.com>
2021-06-23 11:39:10 +0100

    Update UFC.ino

--- Modules/UFC/UFC.ino
+++ Modules/UFC/UFC.ino
@@ -1,3 +1,12 @@
+/*
+  Description: Arduino Code for the custom Up Front Controller (UFC)
+  
+  Author: James Hall
+  Created: 23/06/21
+
+  Platform:
+  This should work on any Arduino that has an ATMega328 controller
+  (Uno, Pro Mini, many others).
  */
 
 void setup() {
