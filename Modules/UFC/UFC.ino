239c5c3dcc9fb53cf2bfe8b5b31fb1876a684b6c
James Hall <62351162+jameshall87@users.noreply.github.com>
2021-06-23 17:22:07 +0100

    Update UFC.ino
    Added the definition of a 8x8 'keypad'
    Void Keypad created as test

--- Modules/UFC/UFC.ino
+++ Modules/UFC/UFC.ino
@@ -15,9 +15,32 @@
 
 //Includes
 #include "DcsBios.h"
+#include <Keypad.h>
 
 //Registry
 DcsBios::LED masterCautionLt(0x7408, 0x0200, OnBoardLED);
+
+//Create the keypad Matrix for button inputs
+const byte ROWS = 8; // eight rows
+const byte COLS = 8; // eight columns
+// Define the Keymap
+char keys[ROWS][COLS] = {
+  {'1','2','3','\0','\0','\0','\0','\0'},
+  {'4','5','6','\0','\0','\0','\0','\0'},
+  {'7','8','9','\0','\0','\0','\0','\0'},
+  {'C','0','E','\0','\0','\0','\0','\0'},
+  {'\0','\0','\0','\0','\0','\0','\0','\0'},
+  {'\0','\0','\0','\0','\0','\0','\0','\0'},
+  {'\0','\0','\0','\0','\0','\0','\0','\0'},
+  {'\0','\0','\0','\0','\0','\0','\0','\0'}
+};
+
+// Connect keypad to these Arduino pins.
+byte rowPins[ROWS] = { 37, 36, 35, 34, 33, 32, 31, 30 };
+byte colPins[COLS] = { 22, 23, 24, 25, 26, 27, 28, 29 }; 
+
+// Create the Keypad
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup() {
   DcsBios::setup(); 
@@ -25,4 +48,24 @@ void setup() {
 
 void loop() {
   DcsBios::loop();
+  keypad();
 }
+
+void keypad(){
+  char key = kpd.getKey();
+  if(key)  // Check for a valid key.
+  {
+    switch (key)
+    {
+      case '*':
+        digitalWrite(OnBoardLED, LOW);
+        break;
+      case '#':
+        digitalWrite(OnBoardLED, HIGH);
+        break;
+      default:
+        delay(10);
+        //Serial.println(key);
+    }
+  }
+ }
