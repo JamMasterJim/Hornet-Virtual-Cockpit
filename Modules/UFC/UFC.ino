3dbebce0594f6e264ebd9a15176f89fe95f7f850
James Hall <62351162+jameshall87@users.noreply.github.com>
2021-07-06 23:44:21 +0100

    Update UFC.ino

--- Modules/UFC/UFC.ino
+++ Modules/UFC/UFC.ino
@@ -14,90 +14,128 @@
 #define OnBoardLED 13
 
 //DCS Bios Switch2Pos
-#define UFC_COMM1_PULL 1
-#define UFC_COMM2_PULL 0
+#define UFC_COMM1_PULL 4
+#define UFC_COMM2_PULL 5
 
 //DCS Bios Switch3Pos
 
 //DCS Bios RotaryEncoders
+#define UFC_COMM1_CHANNEL_SELECT_CLK 2
+#define UFC_COMM1_CHANNEL_SELECT_DT 3
+#define UFC_COMM2_CHANNEL_SELECT_CLK 6
+#define UFC_COMM2_CHANNEL_SELECT_DT 7
 
 //DCS Bios Potentiometers
 
 //DCS Bios LED
 
 //Includes
-#include "DcsBios.h"
+//#include "DcsBios.h"
 
 //DCS Bios Register
 //Switch2Pos
-DcsBios::Switch2Pos ufcComm1Pull("UFC_COMM1_PULL", UFC_COMM1_PULL);
-DcsBios::Switch2Pos ufcComm2Pull("UFC_COMM2_PULL", UFC_COMM2_PULL);
+//DcsBios::Switch2Pos ufcComm1Pull("UFC_COMM1_PULL", UFC_COMM1_PULL);
+//DcsBios::Switch2Pos ufcComm2Pull("UFC_COMM2_PULL", UFC_COMM2_PULL);
 
 //Switch3Pos
 
 //RotaryEncoder
+//DcsBios::RotaryEncoder ufcComm1ChannelSelect("UFC_COMM1_CHANNEL_SELECT", "DEC", "INC", UFC_COMM1_CHANNEL_SELECT_DT, UFC_COMM1_CHANNEL_SELECT_CLK);
+//DcsBios::RotaryEncoder ufcComm2ChannelSelect("UFC_COMM2_CHANNEL_SELECT", "DEC", "INC", UFC_COMM2_CHANNEL_SELECT_DT, UFC_COMM2_CHANNEL_SELECT_CLK);
 
 //Potentiometer
 
 //LED
 
+
+//Variables
+
+
 void setup() {
+  /*
   DcsBios::setup();
+
+  pinMode(UFC_COMM1_PULL, INPUT_PULLUP);
+  pinMode(UFC_COMM2_PULL, INPUT_PULLUP);
+  */
+  
+  Serial.begin(9600);
+
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
   
   //Setup the interups 
-  PCICR |= B00000111;      //Bit0 = 1 -> "PCIE0" enabeled, Bit1 = 1 -> "PCIE1" enabeled, Bit2 = 1 -> "PCIE2" enabeled
-  PCMSK0 |= B00000000;      //Nothin will trigger Interupt
+  PCICR |= B00000001;      //Bit0 = 1 -> "PCIE0" enabeled, Bit1 = 0 -> "PCIE1" disabeled, Bit2 = 0 -> "PCIE2" disabeled
+  PCMSK0 |= B00000011;      //Nothin will trigger Interupt
   PCMSK1 |= B00000000;      //Nothin will trigger Interupt
   PCMSK2 |= B00000000;      //Nothin will trigger Interupt
+  
 }
 
 void loop() {
-  DcsBios::loop();
+  //DcsBios::loop();
 }
 
 //Interrupt Services
 ISR (PCINT0_vect) 
-{
-  /*
-   * ---------- THIS CODE IS FAULTLY ----------  
-   * NOTE Maybe switch the Debounce inside the Rising/Falling Edge detection???
-  //for PCINT[0-7] 
+{ 
+  //for PCINT[0-7]   
   static unsigned long last_interrupt_time = 0;
   unsigned long interrupt_time = millis();
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
-  if (interrupt_time - last_interrupt_time > 100) 
+  if (interrupt_time - last_interrupt_time > 200) 
   {
-    if(!digitalRead(C1) && C1_state)
-    {
-      C1_state = LOW;
-      Serial.println("C1 Falling");
-    }
-    else if(digitalRead(C1) && !C1_state)
-    {
-      C1_state = HIGH;
-      Serial.println("C1 Rising");
-    }
-
-    if(!digitalRead(C2) && C2_state)
-    {
-      C2_state = LOW;
-      Serial.println("C2 Falling");
-    }
-    else if(digitalRead(C2) && !C2_state)
-    {
-      C2_state = HIGH;
-      Serial.println("C2 Rising");
-    }
-  }
+    char column = '\0';
+    column = findColumn (PINB);
+  } 
   last_interrupt_time = interrupt_time;
-  Serial.println(last_interrupt_time);
-  */
 }
  
-ISR (PCINT1_vect) {
+ISR (PCINT1_vect) 
+{
   //for PCINT[8-15] Note PCINT[11-15] not exposed physically
- }
+}
  
-ISR (PCINT2_vect) {
+ISR (PCINT2_vect) 
+{
   //for PCINT[16-23] 
- }
+}
+
+char findColumn (byte port)
+{
+    switch (port) //must be a better way of doing this... its grim!
+    {
+      case 0b11111110:
+      return '0';
+      break;
+      case 0b11111101:
+      return '1';
+      break;
+      case 0b11111011:
+      return '2';
+      break;
+      case 0b11110111:
+      return '3';
+      break;
+      case 0b11101110:
+      return '4';
+      break;
+      case 0b11011101:
+      return '5';
+      break;
+      case 0b10111111:
+      return '6';
+      break;
+      case 0b01111111:
+      return '7';
+      break;
+      default:
+      return '\0';
+      break;
+    }
+}
