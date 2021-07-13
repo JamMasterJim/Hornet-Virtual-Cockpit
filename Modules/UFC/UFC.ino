0f741d5a16e069d2bdb182799c6372567ee3af2b
James Hall <62351162+jameshall87@users.noreply.github.com>
2021-07-13 15:40:24 +0100

    Update UFC.ino

--- Modules/UFC/UFC.ino
+++ Modules/UFC/UFC.ino
@@ -13,8 +13,11 @@ int columns []={62, 63, 64 ,65 ,66, 67, 68, 69}; //Array of digital pins used as
 int rows []={37, 36, 35, 34, 33, 32, 31, 30}; //Array of digital pins used as rows for the digital matrix
 int UFCBrt = 0;
 int UFCBrtPWM = 0;
+const char True[] = "1";
+const char False[] = "0"; 
 
 #define NOP __asm__ __volatile__ ("nop\n\t")
+
 
 //Definitions
 #define DCSBIOS_IRQ_SERIAL
@@ -53,7 +56,7 @@ DcsBios::RotaryEncoder ufcComm1ChannelSelect("UFC_COMM1_CHANNEL_SELECT", "DEC", 
 DcsBios::RotaryEncoder ufcComm2ChannelSelect("UFC_COMM2_CHANNEL_SELECT", "DEC", "INC", UFC_COMM2_CHANNEL_SELECT_DT, UFC_COMM2_CHANNEL_SELECT_CLK);
 
 //Potentiometer
-DcsBios::PotentiometerEWMA<5, 128, 5> ufcBrt("UFC_BRT", UFC_BRT);
+//DcsBios::PotentiometerEWMA<5, 128, 5> ufcBrt("UFC_BRT", UFC_BRT);
 
 //LED
 
@@ -64,9 +67,11 @@ void onUfcBrtChange(unsigned int newValue) {
 DcsBios::IntegerBuffer ufcBrtBuffer(0x741e, 0xffff, 0, onUfcBrtChange);
 
 
+
 void setup() {
 
   DcsBios::setup();
+  
   /*
    * --Help--
    * DDxn
@@ -113,8 +118,8 @@ void setup() {
 
 void loop() {
   DcsBios::loop();
-  UFCBrtPWM = map(UFCBrt, 0, 65535, 0, 255);
-  analogWrite(UFC_BRT_PWM, UFCBrtPWM);
+  //UFCBrtPWM = map(UFCBrt, 0, 65535, 0, 255);
+  //analogWrite(UFC_BRT_PWM, UFCBrtPWM);
 }
 
 //Interrupt Services
@@ -134,18 +139,19 @@ ISR (PCINT2_vect)
   //for PCINT[16-23] --> Port K
   static unsigned long last_interrupt_time = 0;
   unsigned long interrupt_time = millis();
+  static unsigned int button = 0;
+  unsigned int PortK = 0;
+  unsigned int PortC = 0;
   
-  // If interrupts come faster than 100ms, assume it's a bounce and ignore
+  // If interrupts come faster than 20ms, assume it's a bounce and ignore
   //Also ignore any return to 1 i.e. button release
-  if (interrupt_time - last_interrupt_time > 100 && PINK != 0b11111111) 
+  if (interrupt_time - last_interrupt_time > 10 && PINK != 0b11111111) 
   {
-    //Serial.println("Button Pressed...");
-    char column = '\0';
-    char row = '\0';
-    
-    column = findColumn (PINK);
-    row = findRow (column);
 
+    PortK = ~PINK & 0b0000000011111111;
+    PortC = ~findRow() << 8;
+    button = PortC + PortK;
+    
     //Reset ports K = Input(Pull-up) C = Output(Low)
     DDRK  = B00000000;  //Configure as Input
     PORTK = B11111111;  //Enable Internal Pull-up  
@@ -153,54 +159,20 @@ ISR (PCINT2_vect)
     DDRC  = B11111111;  //Configure as Output  
     PORTC = B00000000;  //Output low (Sink)
 
-    //sendDcsBiosMessage("UFC_AP", "1");
-    
-    //Serial.print("Column: ");
-    //Serial.println(column);
-    //Serial.print("Row: ");
-    //Serial.println(row);
+    buttonPress(button, True);
   } 
-  
+
+  if (interrupt_time - last_interrupt_time > 10 && PINK == 0b11111111)
+  {
+    buttonPress(button, False);
+  }
+
   last_interrupt_time = interrupt_time;
 }
 
-char findColumn (byte port)
-{
-    //Serial.print("Port K: ");
-    //Serial.println(port);
-    switch (port) //must be a better way of doing this... its grim!
-    {
-      case 0b11111110:
-      return '0';
-      break;
-      case 0b11111101:
-      return '1';
-      break;
-      case 0b11111011:
-      return '2';
-      break;
-      case 0b11110111:
-      return '3';
-      break;
-      case 0b11101111:
-      return '4';
-      break;
-      case 0b11011111:
-      return '5';
-      break;
-      case 0b10111111:
-      return '6';
-      break;
-      case 0b01111111:
-      return '7';
-      break;
-      default:
-      return '\0';
-      break;
-    }
-}
 
-char findRow (char column)
+
+unsigned int findRow ()
 {
 
   DDRC  = B00000000;  //Change Port C from output to input 
@@ -210,38 +182,171 @@ char findRow (char column)
   PORTK = B00000000;  //Output low (Sink)
 
   NOP;
-  
-  //Serial.print("Port C: ");
-  //Serial.println(PINC);
-  
-    switch (PINC)
+
+  return PINC;
+}
+
+void buttonPress (unsigned int button, const char *value)
+{
+    switch (button)
     {
-      case 0b11111110:
-      return '0';
+      //Row 0
+      case 257:
+        //Keyboard 1
+        sendDcsBiosMessage("UFC_1", value);
       break;
-      case 0b11111101:
-      return '1';
+      case 258:
+        //Keyboard 2
+        sendDcsBiosMessage("UFC_2", value);
       break;
-      case 0b11111011:
-      return '2';
+      case 260:
+        //Keyboard 3
+        sendDcsBiosMessage("UFC_3", value);
       break;
-      case 0b11110111:
-      return '3';
+      case 264:
+        //NC
       break;
-      case 0b11101111:
-      return '4';
+      case 272:
+        //Option Select 1
+        sendDcsBiosMessage("UFC_OS1", value);
       break;
-      case 0b11011111:
-      return '5';
+      case 288:
+        //Function A/P
+        sendDcsBiosMessage("UFC_AP", value);
       break;
-      case 0b10111111:
-      return '6';
+      case 320:
+        //Function IFF
+        sendDcsBiosMessage("UFC_IFF", value);
       break;
-      case 0b01111111:
-      return '7';
+      case 384:
+        //NC
       break;
+
+      //Row 1
+      case 513:
+        //Keyboard 4
+        sendDcsBiosMessage("UFC_4", value);
+      break;
+      case 514:
+        //Keyboard 5
+        sendDcsBiosMessage("UFC_5", value);
+      break;
+      case 516:
+        //Keyboard 6
+        sendDcsBiosMessage("UFC_6", value);
+      break;
+      case 520:
+        //NC
+      break;
+      case 528:
+        //Option Select 2
+        sendDcsBiosMessage("UFC_OS2", value);
+      break;
+      case 544:
+        //Function TCN
+        sendDcsBiosMessage("UFC_TCN", value);
+      break;
+      case 576:
+        //Function ILS
+        sendDcsBiosMessage("UFC_ILS", value);
+      break;
+      case 640:
+        //NC
+      break;
+      
+      //Row 2
+      case 1025:
+        //Keyboard 7
+        sendDcsBiosMessage("UFC_7", value);
+      break;
+      case 1026:
+        //Keyboard 8
+        sendDcsBiosMessage("UFC_8", value);
+      break;
+      case 1028:
+        //Keyboard 9
+        sendDcsBiosMessage("UFC_9", value);
+      break;
+      case 1032:
+        //NC
+      break;
+      case 1040:
+        //Option Select 3
+        sendDcsBiosMessage("UFC_OS3", value);
+      break;
+      case 1056:
+        //Function DL
+        sendDcsBiosMessage("UFC_DL", value);
+      break;
+      case 1088:
+        //Function BCN
+        sendDcsBiosMessage("UFC_BCN", value);
+      break;
+      case 1152:
+        //NC
+      break;
+
+      //Row 3
+      case 2049:
+        //Keyboard CLR
+        sendDcsBiosMessage("UFC_CLR", value);
+      break;
+      case 2050:
+        //Keyboard 0
+        sendDcsBiosMessage("UFC_0", value);
+      break;
+      case 2052:
+        //Keyboard ENT
+        sendDcsBiosMessage("UFC_ENT", value);
+      break;
+      case 2056:
+        //NC
+      break;
+      case 2064:
+        //Option Select 4
+        sendDcsBiosMessage("UFC_OS4", value);
+      break;
+      case 2080:
+        //Function ON/OFF
+        sendDcsBiosMessage("UFC_ONOFF", value);
+      break;
+      case 2112:
+        //Emission Control
+        sendDcsBiosMessage("UFC_EMCON", value);
+      break;
+      case 2176:
+        //NC
+      break;
+
+      //Row 4
+      case 4097:
+        //NC
+      break;
+      case 4098:
+        //NC
+      break;
+      case 4100:
+        //NC
+      break;
+      case 4104:
+        //NC
+      break;
+      case 4112:
+        //Option Select 5
+        sendDcsBiosMessage("UFC_OS5", value);
+      break;
+      case 4128:
+        //NC
+      break;
+      case 4160:
+        //I/P
+        sendDcsBiosMessage("UFC_IP", value);
+      break;
+      case 4224:
+        //NC
+      break;
+     
       default:
-      return '\0';
       break;
     }
 }
